6b1d95aee7ff0ebb4af26e26958b150bd1064d9a
Marcin Iwanicki <marcin.iwanicki@appliwings.com>
2018-03-24 23:52:48 +0000

    Init project.

---
+++ Sources/StateGenerator.h
@@ -0,0 +1,19 @@
+//
+//  StateGenerator.h
+//  StateGenerator
+//
+//  Created by Marcin Iwanicki on 24/03/2018.
+//  Copyright © 2018 Marcin Iwanicki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StateGenerator.
+FOUNDATION_EXPORT double StateGeneratorVersionNumber;
+
+//! Project version string for StateGenerator.
+FOUNDATION_EXPORT const unsigned char StateGeneratorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StateGenerator/PublicHeader.h>
+
+
