931196a8241fece8fcdb3382bf4f8651a865a79a
Marcin Iwanicki <marcin.iwanicki@appliwings.com>
2018-03-25 00:04:02 +0000

    Add build script.

---
+++ Scripts/build_and_test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+if [ ! -d Sources ]; then
+  cd ..
+fi
+
+xcodebuild -workspace StateGenerator.xcworkspace -scheme StateGenerator -sdk iphonesimulator build test -destination "platform=iOS Simulator,name=iPhone SE" | xcpretty -c
